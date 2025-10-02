69688f3d656d65422458e3a959af1a4fbc654ebc
Utkarsh Pandey <utk2665@gmail.com>
2025-10-02 22:29:30 +0530

    auth.ts for obtaining the token

---
+++ src/lib/auth.ts
@@ -0,0 +1,57 @@
+//File to get shit like Current User ID and other auth related stuff
+
+import { NextRequest } from "next/server";
+import jwt from "jsonwebtoken";
+
+export function getCurrentUserId(request: NextRequest): string {
+
+    //get token from cookie or header
+
+    const token = request.cookies.get("token")?.value || 
+                request.headers.get("Authorization")?.replace("Bearer ", "");
+
+    if(!token){
+        throw new Error("No token provided");
+    }
+
+    try {
+
+        if(!process.env.JWT_SECRET){
+            throw new Error("JWT_SECRET is not defined");
+        }
+        
+        const decoded = jwt.verify(token, process.env.JWT_SECRET) as any;
+
+        if(!decoded || !decoded.userId){
+            throw new Error("Invalid authentication token");
+        }
+        return decoded.userId;
+
+    } catch (error) {
+        
+        throw new Error("Invalid authentication token");
+    }
+
+}
+
+export function getCurrentUserIdFromCookie(request: NextRequest): string {
+
+    const token = request.cookies.get("token")?.value;
+
+    if(!token){
+        throw new Error("No authentication token provided");
+    }
+
+    try {
+
+        if(!process.env.JWT_SECRET){
+            throw new Error("JWT_SECRET is not defined");
+        }
+        
+        const decoded = jwt.verify(token, process.env.JWT_SECRET) || 'somesecretkeynig' as any;
+        return decoded.userId;
+
+    } catch (error) {
+        throw new Error("Invalid authentication token");
+    }
+}
