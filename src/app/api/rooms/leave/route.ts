b0235ca0cd14bc73e7bd6d91722019877dd216c5
Utkarsh Pandey <utk2665@gmail.com>
2025-10-10 14:06:49 +0530

    /leave endpoint

---
+++ src/app/api/rooms/leave/route.ts
@@ -0,0 +1,71 @@
+import { PrismaClient } from "@/generated/prisma";
+import { getCurrentUserIdFromRequest } from "@/lib/auth";
+import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function POST(request: NextRequest){
+
+    try {
+        
+
+        const { roomCode } = await request.json();
+
+        const userId = getCurrentUserIdFromRequest(request);
+
+        //find room and user participation
+        const room = await prisma.room.findFirst({
+            where: {
+                code: roomCode,
+            }, include: {
+                participants: true,
+            }
+        });
+
+        if(!room){
+            return NextResponse.json({
+                error: "Room not found"
+            }, {
+                status: 404,
+            })
+        };
+
+        //remove users from room
+        await prisma.roomUser.deleteMany({
+            where: {
+                roomId: room.id,
+                userId,
+            }
+        });
+
+        //if this was the last participant and not the host, cleanup the room
+        const remainingParticipants = room.participants.filter(p => p.userId !== userId); //not the host
+        if(remainingParticipants.length === 0){
+            await prisma.room.delete({
+                where: {
+                    id: room.id,
+                }, data: {
+                    isActive: false,
+                }
+            });
+        }
+
+        return NextResponse.json({
+            message: "You have left the room"
+        }, {
+            status: 200,
+        });
+
+
+    } catch (error) {
+        
+        console.error("Error leaving the room: ", error);
+        return NextResponse.json({
+            error: "Failed to leave the room"
+        }, {
+            status: 500, 
+        })
+    }finally{
+        await prisma.$disconnect();
+    }
+}
